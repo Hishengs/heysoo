c65828679f16b3a598a1296258b9a8a781b7a9b8
Hishengs <819537918@qq.com>
2017-09-10 15:58:29 +0800

    add koa extend

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,14 @@
-const Heysoo = require('../index.js')
+require('babel-core/register')({
+  // "presets": ['es2015'],
+  "plugins": ["transform-async-to-generator"]
+});
 
-const app = new Heysoo()
+const Heysoo = require('../index.js');
+
+const app = new Heysoo();
 
 /*app.use(async ctx => {
   ctx.body = 'hello, world.';
 });*/
 
-app.start()
+app.start();
