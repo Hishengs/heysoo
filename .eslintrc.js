8f6ec392d74ff0db5ec18a866eb06d6d58e11ac5
Hishengs <819537918@qq.com>
2018-04-04 22:37:44 +0800

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  "extends": "bitpower"
+  "extends": "bitpower",
+  "rules": {
+    "space-before-function-paren": "off",
+    "no-restricted-syntax": "off",
+    "no-await-in-loop": "warn",
+    "import/newline-after-import": "warn",
+  },
 };
