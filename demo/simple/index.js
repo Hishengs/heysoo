35af32caaeacf06f8fd93678b66294b2384ca9ab
Hishengs <819537918@qq.com>
2018-06-25 00:45:56 +0800

    perf: show warning message instead of throw an error if no router.js

--- demo/simple/index.js
+++ demo/simple/index.js
@@ -2,6 +2,10 @@ const Heysoo = require('../../index.js');
 
 const app = new Heysoo();
 
+// app.use(async (ctx) => {
+//   ctx.body = 'hello, world.';
+// });
+
 app.start(() => {
   app.console.info('server is start');
 });
